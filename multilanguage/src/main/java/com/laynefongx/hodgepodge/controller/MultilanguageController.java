8e6d642dda03a982f2b9855759d8db32a7502b5b
LayneFongX <laynefongx@gmail.com>
2022-02-14 16:10:10 +0800

    提交代码

--- multilanguage/src/main/java/com/laynefongx/hodgepodge/controller/MultilanguageController.java
+++ multilanguage/src/main/java/com/laynefongx/hodgepodge/controller/MultilanguageController.java
@@ -1,6 +1,7 @@
 package com.laynefongx.hodgepodge.controller;
 
 
+import com.laynefongx.hodgepodge.domain.operate.FileResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,33 @@ public class MultilanguageController {
         return "Hello";
     }
 
+    @GetMapping("/getWiserAppCompareResult")
+    public FileResult getWiserAppCompareResult() {
+        return null;
+    }
 
+    @GetMapping("/getDeviceCompareResult")
+    public FileResult getDeviceCompareResult() {
+        return null;
+    }
+
+    @GetMapping("/getFAQCompareResult")
+    public FileResult getFAQCompareResult() {
+        return null;
+    }
+
+    @GetMapping("/getWiserAppMergeResult")
+    public FileResult getWiserAppMergeResult() {
+        return null;
+    }
+
+    @GetMapping("/getDeviceMergeResult")
+    public FileResult getDeviceMergeResult() {
+        return null;
+    }
+
+    @GetMapping("/getFAQMergeResult")
+    public FileResult getFAQMergeResult() {
+        return null;
+    }
 }
